d2353c9783fefed16d87e57f17ddaa5aaca38028
Yuki Sato <sato@momiji.arc.cs.okayama-u.ac.jp>
2018-01-30 23:13:06 +0900

    Check Argument and Define Condition Code.

--- flopoco_test_generator.c
+++ flopoco_test_generator.c
@@ -97,6 +97,13 @@ int main(int ac, char *av[]){
 
   int i;
 
+  int line=0;             /* Input file's line */
+  int in_flag=0;
+  int out_flag=0;
+
+  int inf_flag=0;
+  int nan_flag=0;
+
   zero_frac = (char *)calloc(frac+1, sizeof(char));
   zero_exp = (char *)calloc(exp+1, sizeof(char));
   inf = (char *)calloc(exp+1, sizeof(char));
@@ -107,54 +114,127 @@ int main(int ac, char *av[]){
  
   while (fgets(str, 64, fp) != NULL){
     
-    a = (char *)calloc(bit_width*2, sizeof(char));
+    line++;
+    
+    if(line%2==1){
+      in_flag = 1; out_flag = 0;
+    }else{
+      in_flag = 0; out_flag = 1;
+    }      
+
     exp_a = (char *)calloc(exp+1, sizeof(char));
     frac_a = (char *)calloc(frac+1, sizeof(char));
     in_a = (char *)calloc(bit_width*2, sizeof(char));
         
     data = split(str);
 
-    for (i=0;i<data.d_c;i++){
+    if(in_flag){
+      for (i=0;i<data.d_c;i++){
+	a = (char *)calloc(bit_width*2, sizeof(char));
+	
+	strcpy(a, data.data[i]);
+	
+	strncpy(exp_a, a+1, exp);
+	strncpy(frac_a, a+1+exp, frac);
+	
+	if(!strncmp(exp_a, zero_exp, bit_width)){
+	  if(!strncmp(frac_a, zero_frac, bit_width)){
+	    str_front_push(a, '0');
+	    str_front_push(a, '0');
+	    strcpy(in_a, a);
+	  }
+	  else{
+	    str_front_push(a, '1');
+	    str_front_push(a, '0');
+	    strcpy(in_a, a);
+	  }
+	}else if(!strncmp(exp_a, inf, bit_width)){
+	  if(!strncmp(frac_a, zero_frac, bit_width)){
+	    str_front_push(a, '0');
+	    str_front_push(a, '1');
+	    strcpy(in_a, a);
+	    inf_flag = 1;
+	  }
+	  else{
+	    str_front_push(a, '1');
+	    str_front_push(a, '1');
+	    strcpy(in_a, a);
+	    nan_flag=1;
+	  }
+	}else if(a[1] == NULL){
+	  strcpy(in_a, a);      
+	}else{
+	  str_front_push(a, '1');
+	  str_front_push(a, '0');
+	  strcpy(in_a, a);
+	}
+	
+ 	  gmp_printf("%s ", in_a);  
 
-      strcpy(a, data.data[i]);
-    
-      strncpy(exp_a, a+1, exp);
-      strncpy(frac_a, a+1+exp, frac);
-    
-      if(!strncmp(exp_a, zero_exp, bit_width)){
-	if(!strncmp(frac_a, zero_frac, bit_width)){
-	  str_front_push(a, '0');
-	  str_front_push(a, '0');
+	free(a);
+    }    
+
+      printf("\n");
+
+    }else if(out_flag){
+      for (i=0;i<data.d_c;i++){
+	a = (char *)calloc(bit_width*2, sizeof(char));
+	
+	strcpy(a, data.data[i]);
+
+	strncpy(exp_a, a+1, exp);
+	strncpy(frac_a, a+1+exp, frac);
+
+	if(strlen(a) != exp+frac+1){
 	  strcpy(in_a, a);
-	}
-	else{
+	}else if(inf_flag){
+	  str_front_push(a, '0');
+	  str_front_push(a, '1');
+	  strcpy(in_a, a);
+	  inf_flag = 0;
+	}else if(nan_flag){
+	  str_front_push(a, '1');
+	  str_front_push(a, '1');
+	  strcpy(in_a, a);
+	  nan_flag = 0;
+	}else if(!strncmp(exp_a, zero_exp, bit_width)){
+	  if(!strncmp(frac_a, zero_frac, bit_width)){
+	    str_front_push(a, '0');
+	    str_front_push(a, '0');
+	    strcpy(in_a, a);
+	  }
+	  else{
+	    str_front_push(a, '1');
+	    str_front_push(a, '0');
+	    strcpy(in_a, a);
+	  }
+	}else if(!strncmp(exp_a, inf, bit_width)){
+	  if(!strncmp(frac_a, zero_frac, bit_width)){
+	    str_front_push(a, '0');
+	    str_front_push(a, '1');
+	    strcpy(in_a, a);
+	    inf_flag=0;
+	  }
+	  else{
+	    str_front_push(a, '1');
+	    str_front_push(a, '1');
+	    strcpy(in_a, a);
+	    nan_flag=0;
+	  }	  
+	}else{
 	  str_front_push(a, '1');
 	  str_front_push(a, '0');
 	  strcpy(in_a, a);
 	}
-      }else if(!strncmp(exp_a, inf, bit_width)){
-	if(!strncmp(frac_a, zero_frac, bit_width)){
-	  str_front_push(a, '0');
-	  str_front_push(a, '1');
-	  strcpy(in_a, a);
-	}
-	else{
-	  str_front_push(a, '1');
-	  str_front_push(a, '1');
-	  strcpy(in_a, a);
-	}
-      }else if(a[1] == NULL){
-	strcpy(in_a, a);      
-      }else{
-	str_front_push(a, '1');
-	str_front_push(a, '0');
-	strcpy(in_a, a);
+
+	gmp_printf("%s ", in_a);  
+
+	free(a);
       }
-
-      gmp_printf("%s ", in_a);  
-    }
     
-    printf("\n");
+      printf("\n");      
+    }
+
 
     free(exp_a);
     free(frac_a);
@@ -167,5 +247,6 @@ int main(int ac, char *av[]){
 
   fclose(fp);
   
+
   return 0;  
 }
